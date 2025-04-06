500088856b0f193bbe04bdb5931cc0ae28b0c93a
Henry <hlsoftdev@proton.me>
2025-04-06 10:02:58 -0600

    Update Reviews.js

--- Reviews.js
+++ Reviews.js
@@ -1,12 +1,29 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-mongoose.connect(process.env.DB);
+mongoose.Promise = global.Promise;
 
-// Movie schema
+try {
+    mongoose.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true });
+    mongoose.set('useCreateIndex', true);
+    mongoose.set('useFindAndModify', false);
+    console.log("MongoDB connection successful");
+} catch (error) {
+    console.log("MongoDB connection error: " + error);
+}
+
+// Review schema
 var ReviewSchema = new Schema({
-
+    movieId: { type: mongoose.Schema.Types.ObjectId, ref: 'Movie', required: true },
+    username: { type: String, required: true },
+    review: { type: String, required: true },
+    rating: { 
+        type: Number, 
+        required: true, 
+        min: [0, 'Rating must be between 0 and 5'], 
+        max: [5, 'Rating must be between 0 and 5'] 
+    }
 });
 
-// return the model
+// Return the model
 module.exports = mongoose.model('Review', ReviewSchema);
